eb965bdb783272fb3980601dae594143d3d843ab
yacara15 <rcyales@gmail.com>
2015-03-24 14:16:14 +0000

    Add javascript for colored map with tooltips

--- js/topo_world.js
+++ js/topo_world.js
@@ -1,8 +1,10 @@
 function generateMap(){
-    var width = 1200,
-        height = 1600;
+    var width = 1800,
+        height = 2200;
     
-    var projection = d3.geo.equirectangular();
+    var projection = d3.geo.equirectangular()
+        .scale(175),
+        color = d3.scale.category20();
 
         var path = d3.geo.path()
             .projection(projection);
@@ -11,15 +13,39 @@ function generateMap(){
         .attr("width", width)
         .attr("height", height);
 
-    d3.json("data/topoworld.json", function(error, topology) {
+    d3.json("data/world_countries_topo.json", function(error, topology) {
+        var countries = topojson.feature(topology, topology.objects.countries).features,
+            neighbors = topojson.neighbors(topology.objects.countries.geometries);
         
-        svg.selectAll(".topoworld")
-            .data(topojson.feature(topology, topology.objects.topoworld).features)
+        svg.selectAll(".countries")
+            .data(countries)
             .enter().append("path")
             .attr("class", "country")
-            .attr("d", path);
+            .attr("d", path)
+            .style("fill", function(d, i) { 
+                return color(d.color = d3.max(neighbors[i], function(n) { 
+                    return countries[n].color;
+                }) + 1 | 0);
+            })
             
-        });
-}
+            .append("title")
+            .text(function(d) { return d.properties.name; });
+            
+            
+            
+         
+            
+            /*svg.selectAll(".country-label")
+            .data(topojson.feature(topology, topology.objects.countries).features)
+            .enter().append("text")
+            .attr("class", "country-label")
+            .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
+            .attr("dy", ".35em")
+            .text(function(d) { return d.properties.name; })
+            .attr("x", function(d) { return d.geometry.coordinates[0] > -1 ? 6 : -6; })
+            .style("text-anchor", function(d) { return d.geometry.coordinates[0] > -1 ? "start" : "end"; });*/
+    });
+    
+};
 
 generateMap();
